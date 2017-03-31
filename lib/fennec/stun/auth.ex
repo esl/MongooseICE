4701070b5c2cd0fa42cd64919b3a240c04fe0e75
Rafał Słota <rafal.slota@erlang-solutions.com>
2017-03-31 14:40:52 +0200

    [skip ci] Add missing file

---
+++ lib/fennec/stun/auth.ex
@@ -0,0 +1,66 @@
+defmodule Fennec.STUN.Auth do
+  @moduledoc """
+  This module implements authentication and authorization of the STUN protocol.
+  """
+
+  alias Jerboa.Format.Body.Attribute
+  alias Jerboa.Format.Body.Attribute.{Username, Nonce, Realm}
+  alias Jerboa.Params
+
+  def get_secret do
+    Application.get_env(:fennec, :secret)
+  end
+
+  def authorize(params, _turn_state) do
+    with u = %Username{} <- Params.get_attr(params, Username),
+         r = %Realm{}    <- Params.get_attr(params, Realm) do
+      # Right now, any authenticated user will do
+      {:ok, %Params{params | attributes: params.attributes -- [u, r]}}
+    end
+  end
+
+  def authenticate(params, turn_state) do
+    nonce = turn_state.nonce
+    signed? = params.signed?
+
+    with %Username{}                <- Params.get_attr(params, Username),
+         %Realm{}                   <- Params.get_attr(params, Realm),
+         n = %Nonce{value: ^nonce}  <- Params.get_attr(params, Nonce),
+         true                       <- params.verified? do
+      {:ok, %Params{params | attributes: params.attributes -- [n]}}
+    else
+      false -> # Not verified -> error code 401
+        {:error, error_params(401, params, turn_state)}
+      _ when signed? -> # If message is signed and there are some attributes
+                        # missing, we need to respond with error code 400
+        {:error, error_params(400, params, turn_state)}
+      %Nonce{} -> # Invalid nonce, error code 438
+        {:error, error_params(438, params, turn_state)}
+      nil when not signed? ->
+        {:error, error_params(401, params, turn_state)}
+    end
+  end
+
+  def maybe(action_fun, params, turn_state) do
+    case should_authorize?(params) do
+      true ->
+        action_fun.(params, turn_state)
+      false ->
+        {:ok, params}
+    end
+  end
+
+  defp should_authorize?(params) do
+    should_authorize?(Params.get_class(params), Params.get_method(params))
+  end
+  defp should_authorize?(:request, :allocate), do: true
+  defp should_authorize?(_, _), do: false
+
+  defp error_params(code, params, turn_state) do
+    %Params{params | attributes: [
+      %Attribute.ErrorCode{code: code},
+      %Attribute.Realm{value: turn_state.realm},
+      %Attribute.Nonce{value: turn_state.nonce}
+    ]}
+  end
+end
