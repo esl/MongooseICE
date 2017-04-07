72044417f31a75092d23e63a924b18684a795c1e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-04-07 17:13:44 +0200

    Implement allocation mismatch check

--- lib/fennec/evaluator/refresh/request.ex
+++ lib/fennec/evaluator/refresh/request.ex
@@ -5,21 +5,40 @@ defmodule Fennec.Evaluator.Refresh.Request do
   [rfc5766-sec7]: https://tools.ietf.org/html/rfc5766#section-7
   """
 
+  import Fennec.Evaluator.Helper, only: [maybe: 3]
   alias Fennec.{TURN, TURN.Allocation}
-  alias Jerboa.Format.Body.Attribute.Lifetime
+  alias Jerboa.Format.Body.Attribute.{ErrorCode, Lifetime}
   alias Jerboa.Params
 
   @spec service(Params.t, Fennec.client_info, Fennec.UDP.server_opts, TURN.t)
     :: {Params.t, TURN.t}
   def service(params, _client, _server, turn_state) do
-    ## TODO: match on duration!
-    %Lifetime{duration: 0} = Params.get_attr(params, Lifetime)
-    case turn_state.allocation do
-      nil ->
-        {params, turn_state}
-      a ->
+    status =
+      {:continue, params, %{}}
+      |> maybe(&allocation_mismatch/3, [turn_state])
+      |> maybe(&refresh/3, [turn_state])
+    case status do
+      {:error, code} ->
+        {%{params | attributes: [code]}, turn_state}
+      {:respond, {new_params, new_turn_state}} ->
+        {new_params, new_turn_state}
+    end
+  end
+
+  defp allocation_mismatch(_params, _state, %TURN{allocation: nil}) do
+    {:error, %ErrorCode{name: :allocation_mismatch}}
+  end
+  defp allocation_mismatch(params, state, %TURN{allocation: _}) do
+    {:continue, params, state}
+  end
+
+  defp refresh(params, _state, %TURN{allocation: a} = t) do
+    case Params.get_attr(params, Lifetime) do
+      %Lifetime{duration: 0} ->
         new_a = %Allocation{ a | expire_at: 0 }
-        {params, %TURN{ turn_state | allocation: new_a }}
+        {:respond, {params, %TURN{ t | allocation: new_a }}}
+      %Lifetime{duration: _} ->
+        :erlang.error("not-implemented")
     end
   end
 
