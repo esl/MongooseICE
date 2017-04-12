c6481e5480683afcda743e9ae53006a80028c03c
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-04-12 16:22:51 +0200

    Test discarding data from peers with no permission

--- test/fennec/udp/data_test.exs
+++ test/fennec/udp/data_test.exs
@@ -15,12 +15,35 @@ defmodule Fennec.UDP.DataTest do
   describe "incoming datagram" do
 
     setup ctx do
-      params = %Params{class: :success} = UDP.allocate(ctx.udp)
+      params = UDP.allocate(ctx.udp)
       {:ok, [relay_sock: Params.get_attr(params, XORRelayedAddress)]}
     end
 
     test "gets discarded when there's no permission for peer", ctx do
-      flunk "not implemented yet"
+      ## given a relay address for a peer with no permission
+      %XORRelayedAddress{address: relay_addr, port: relay_port} = ctx.relay_sock
+      {:ok, peer} = :gen_udp.open(0, [{:active, :false}, :binary])
+      {:ok, peer_port} = :inet.port(peer)
+      self_ = self()
+      with_mock Fennec.UDP.Worker, [:passthrough], [
+        handle_peer_data: fn (:no_permission, ip, port, data, state) ->
+          ## we can't use `assert called ...` as we want to ignore `state`
+          send self_, {:no_permission, ip, port, data}
+          :meck.passthrough([:no_permission, ip, port, data, state])
+        end
+      ] do
+        ## when the peer sends a datagram
+        data = "arbitrary data"
+        :ok = :gen_udp.send(peer, relay_addr, relay_port, data)
+        ## then the datagram gets silently discarded
+        ## we can't use `assert called ...` as we want to ignore `state`
+        receive do
+          {:no_permission, {127, 0, 0, 1}, ^peer_port, ^data} ->
+            :ok
+          after 3000 ->
+            flunk("handle_peer_data timeout")
+          end
+      end
     end
 
   end
