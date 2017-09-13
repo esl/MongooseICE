e638f87418c16429b3cee6035c15fb66ca81f092
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-09-13 12:59:25 +0200

    Load Helper.Macros module before the other ones in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,10 @@
 ExUnit.start(exclude: [:system])
-
 # These files have to be required here, since the code requires ExUnit
-# application to be running while loading (use ExUnit.Case)
+# application to be running while loading (use ExUnit.Case).
+# But first we need to load Helper.Macros module, since the other ones
+# use the macros defined in it.
+Code.require_file "helper/macros.ex", __DIR__
+
 {:ok, files} = File.ls("./test/helper")
 Enum.each files, fn(file) ->
   Code.require_file "helper/#{file}", __DIR__
